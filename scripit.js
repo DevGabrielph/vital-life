282b4d6a36342320a58e96d988c647d3d87557e7
Gabriel Macena <108024946+DevGabrielph@users.noreply.github.com>
2022-10-12 01:52:52 -0300

    a

--- scripit.js
+++ scripit.js
@@ -1,1 +1,45 @@
-alert('oi')
+var homee=document.querySelector('#home')
+var vital= document.querySelector('#vital-life')
+var unidade=document.querySelector('#unidades')
+var serviço= document.querySelector('#serviços')
+function home(){
+       if(homee.style.display='none'){
+        homee.style.display='block'
+        vital.style.display='none'
+        unidade.style.display='none'
+        serviço.style.display='none'
+    }
+} 
+function vitalLife(){
+    if(vital.style.display='none'){
+        vital.style.display='block'
+        homee.style.display='none'
+        unidade.style.display='none'
+        serviço.style.display='none'
+
+
+    }
+
+}
+function unidades(){
+    if(unidade.style.display='none'){
+        unidade.style.display='block'
+        homee.style.display='none'
+        vital.style.display='none'
+        serviço.style.display='none'
+
+
+    }
+
+}
+function serviços(){
+    if(serviço.style.display='none'){
+        serviço.style.display='block'
+        homee.style.display='none'
+        unidade.style.display='none'
+        vital.style.display='none'
+
+
+    }
+
+}
